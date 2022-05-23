cf142f5ac7cc974557bd143e6c94c3f38815c849
leandro virzi <leandro.m.virzi@gmail.com>
2022-05-23 16:15:43 -0300

    cotizador de criptomonedas

---
+++ src/data/monedas.js
@@ -0,0 +1,12 @@
+ // API
+ const monedas = [
+    {   id: 'USD', nombre: 'Dolar de Estados Unidos'    },
+    {   id: 'MXN', nombre: 'Peso Mexicano' }, 
+    {   id: 'EUR', nombre: 'Euro'   },
+    {   id: 'ARS', nombre: 'Peso Argentino' },
+    {   id: 'GBP', nombre: 'Libra Esterlina' }
+]
+
+export {
+    monedas
+}
